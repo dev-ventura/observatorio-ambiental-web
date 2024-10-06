0182ee803da1a2c23cc51202327e7176053e60e7
Jorge Montesinos <osmarmontes50@gmail.com>
2024-10-06 00:27:11 -0500

    fire dots added

--- frontend/src/components/WrapperGlobe.jsx
+++ frontend/src/components/WrapperGlobe.jsx
@@ -2,17 +2,21 @@ import { useState, useEffect, useRef } from "react";
 import Globe from "react-globe.gl";
 import axios from "axios";
 import countries from "../data/countries";
+import fires from "../data/backupFire";
 import { csvToJson } from "../utils/convertData";
 import useWindowSize from "../hooks/useWindowSize";
+import { useNavigate } from "react-router-dom";
 
 const WrapperGlobe = () => {
   const globeEl = useRef();
   // const [countries, setCountries] = useState({ features: [] });
-  const [altitude, setAltitude] = useState(0.1);
+  const [altitude, setAltitude] = useState(0.01);
   const [transitionDuration, setTransitionDuration] = useState(1000);
   const [pointsData, setPointsData] = useState([]);
 
   const { width, height } = useWindowSize();
+
+  const navigate = useNavigate();
 
   const getFires = async () => {
     try {
@@ -24,8 +28,11 @@ const WrapperGlobe = () => {
       const response = await axios.get(URL);
       // console.log("axios",response.data);
       console.log(response.data)
-      const jsonData = csvToJson(response.data);
+      let jsonData = csvToJson(response.data);
       console.log("axios", jsonData);
+      if(jsonData.length === 0){
+        jsonData = csvToJson(fires);
+      }
       setPointsData(jsonData);
     } catch (error) {
       console.log(error);
@@ -38,14 +45,6 @@ const WrapperGlobe = () => {
 
     globeEl.current.pointOfView({ altitude: 4 }, 5000);
 
-    setTimeout(() => {
-      setTransitionDuration(4000);
-      setAltitude(
-        () => (feat) =>
-          Math.max(0.1, Math.sqrt(+feat.properties.POP_EST) * 7e-5)
-      );
-    }, 3000);
-
     getFires();
   }, []);
 
@@ -53,24 +52,23 @@ const WrapperGlobe = () => {
     <div style={{ width: "500px", height: "500px" }}>
       <Globe
         ref={globeEl}
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
-        // polygonsData={countries.features.filter(
-        //   (d) => d.properties.ISO_A2 !== "AQ"
-        // )}
-        // polygonAltitude={altitude}
-        // polygonCapColor={() => "rgba(200, 0, 0, 0.6)"}
-        // polygonSideColor={() => "rgba(0, 100, 0, 0.15)"}
-        //   polygonLabel={({ properties: d }) => `
-        //   <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
-        //   Population: <i>${Math.round(+d.POP_EST / 1e4) / 1e2}M</i>
-
-        // `}
-        //   polygonsTransitionDuration={transitionDuration}
-        //   onPolygonClick={() => {alert("ANIMALITOS")}}
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+        polygonsData={countries.features.filter(
+          (d) => d.properties.ISO_A2 !== "AQ"
+        )}
+        polygonAltitude={altitude}
+        polygonCapColor={() => `#${Math.round(Math.random() * Math.pow(2, 24)).toString(16).padStart(6, '0')}80`}
+        polygonStrokeColor={() => "#111"}
+        polygonSideColor={() => "rgba(0, 100, 0, 0.15)"}
+          polygonLabel={({ properties: d }) => `
+          <b>${d.ADMIN} (${d.ISO_A2})</b>
+        `}
+        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
         pointsData={pointsData}
         width={width * 0.7}
         height={height}
-        pointColor={() => "#FF0000"}
+        pointColor={() => "#ff0000"}
         pointAltitude={0.01}
       />
     </div>
