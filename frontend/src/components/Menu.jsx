50803e8b1cd9d6097c96f564572221fee1ce8949
Jorge Montesinos <osmarmontes50@gmail.com>
2024-10-06 01:49:59 -0500

    menu estilos

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,6 +1,6 @@
 //import React from 'react'
 import "./Menu.css";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 export const Menu = () => {
   return (
@@ -9,21 +9,18 @@ export const Menu = () => {
       <Link to="/">
         <h2 className="titleProject">OBSERVATORIO AMBIENTAL</h2>
       </Link>
-      <Link to="/fire-map">
-        <span className="option">Mapa de incendios</span>
-        <br></br>
-      </Link>
-      <Link to="/air-quality-map">
-        <span className="option">Calidad del aire</span>
-        <br></br>
-      </Link>
-      <Link to="/animal-biodiversity">
-        <span className="option">Biodiversidad afectada</span>
-        <br></br>
-      </Link>
-      <Link to="/news">
+      <NavLink to="/fire-map">
+        <span className="option">Mapa de incendios</span> 
+      </NavLink>
+      <NavLink to="/air-quality-map">
+        <span className="option">Calidad del aire</span>  
+      </NavLink>
+      <NavLink to="/animal-biodiversity">
+        <span className="option">Biodiversidad afectada</span>  
+      </NavLink>
+      <NavLink to="/news">
         <span className="option">Noticias</span>
-      </Link>
+      </NavLink>
     </div>
   );
 };
