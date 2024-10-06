03e2db602e37fde908018a4c5c3aea0b3540fe12
Melany Ventura <melanymarisolventura@gmail.com>
2024-10-06 04:19:28 -0400

    Agregue estilos y formato a la seccion biodiversidad junto al dataset

--- frontend/src/components/AnimalBiodiversity.jsx
+++ frontend/src/components/AnimalBiodiversity.jsx
@@ -1,16 +1,35 @@
 import { Menu } from "./Menu";
+import "./AnimalBiodiversity.css";
+import biodiversity from "../data/biodiversity";
+
 const AnimalBiodiversity = () => {
   return (
     <>
-    <div className="page">
-      <div>
-        <Menu />
+      <div className="page">
+         <div>
+          <Menu />
+         </div>
+         <div>
+         <h2>Biodiversidad afectada en Bolivia</h2>
+         <div className="cards">
+          {biodiversity.animales_afectados.map((animal, index) => (
+          <div key={index} className="newsAnimal">
+          {animal.img_url ? (
+            <img
+              className="imageAnimal"
+              src={animal.img_url}
+            />
+          ) : null}
+          <p className="nameAnimal">{animal.nombre} </p>
+          <p className="regionAnimal">{animal.region} </p> 
+          <p className="typeAnimal">{animal.tipo} </p> 
+          </div>
+         ))}
+         </div>
+         </div>
       </div>
-      <div>
-        <p>Cards de la biodiversidad afectada en Bolivia</p>
-      </div>
-    </div>
-  </>
-  )
-}
+    </>
+  );
+};
+
 export default AnimalBiodiversity;
---
+++ frontend/src/data/biodiversity.js
@@ -0,0 +1,186 @@
+const biodiversity = {
+    "animales_afectados": [
+      {
+        "nombre": "Jaguar",
+        "img_url": "https://bolivia.wcs.org/Portals/14/IMG_0214Christian%20Gutierrez-WCS.jpg",
+        "tipo": "Mamífero",
+        "region": "Amazonía boliviana, Departamento de Pando"
+      },
+      {
+        "nombre": "Caimán negro",
+        "img_url": "https://scontent.flpb3-1.fna.fbcdn.net/v/t1.6435-9/205322633_202432298423611_5194988607376396372_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Oh6rd_ijz8sQ7kNvgEUynF5&_nc_ht=scontent.flpb3-1.fna&_nc_gid=A88ttL7OFgfvuOThVHZhvT5&oh=00_AYDm9h1CSiHGnMPFpoFwAp9ZzS9b6Vhj6kvC-duoeAjOdA&oe=6729820F",
+        "tipo": "Reptil",
+        "region": "Pantanal boliviano, Departamento de Santa Cruz"
+      },
+      {
+        "nombre": "Guacamayo azul y amarillo",
+        "img_url": "https://wikifaunia.com/wp-content/uploads/2013/11/guacamayos.jpg",
+        "tipo": "Ave",
+        "region": "Chiquitania, Departamento de Santa Cruz"
+      },
+      {
+        "nombre": "Oso andino",
+        "img_url": "https://laderasur.com/wp-content/uploads/2021/08/f608x342-302197-331920-27.jpg",
+        "tipo": "Mamífero",
+        "region": "Yungas, Departamento de La Paz"
+      },
+      {
+        "nombre": "Vicuña",
+        "img_url": "https://abi.bo/images/Noticias/Sociedad/dic-22/TURISMO.jpg",
+        "tipo": "Mamífero",
+        "region": "Altiplano, Departamento de Oruro"
+      },
+      {
+        "nombre": "Tapir",
+        "img_url": "https://www.soldepando.com/wp-content/uploads/2013/04/a.jpg",
+        "tipo": "Mamífero",
+        "region": "Amazonía boliviana, Departamento de Beni"
+      },
+      {
+        "nombre": "Cóndor andino",
+        "img_url": "https://www.lostiempos.com/sites/default/files/styles/noticia_detalle/public/ipinimgcom.jpg?itok=N1A8UYvz",
+        "tipo": "Ave",
+        "region": "Cordillera de los Andes, Departamento de Potosí"
+      },
+      {
+        "nombre": "Capibara",
+        "img_url": "https://files.worldwildlife.org/wwfcmsprod/images/Capybara_2_Colombia/story_full_width/6fqwyt59ce_Medium_WW239177.jpg",
+        "tipo": "Mamífero",
+        "region": "Pantanal boliviano, Departamento de Santa Cruz"
+      },
+      {
+        "nombre": "Mono aullador rojo",
+        "img_url": "https://s3.animalia.bio/animals/photos/full/1.25x1/2560px-alouatta-sara-28bolivian-red-howler29.webp?id=d1e511130cdb268ae15f3dfc791ce405",
+        "tipo": "Mamífero",
+        "region": "Amazonía boliviana, Departamento de Pando"
+      },
+      {
+        "nombre": "Flamenco andino",
+        "img_url": "https://inaturalist-open-data.s3.amazonaws.com/photos/171995487/original.jpg",
+        "tipo": "Ave",
+        "region": "Altiplano, Departamento de Potosí"
+      },
+      {
+        "nombre": "Zorro andino",
+        "img_url": "https://saeeg.org/wp-content/uploads/2023/08/CARRANZA-ROMERO-ZORRO-ANDINO.png",
+        "tipo": "Mamífero",
+        "region": "Altiplano, Departamento de La Paz"
+      },
+      {
+        "nombre": "Anaconda amarilla",
+        "img_url": "https://www.soldepando.com/wp-content/uploads/2022/07/La-Anaconda-Amarilla.jpg",
+        "tipo": "Reptil",
+        "region": "Amazonía boliviana, Departamento de Beni"
+      },
+      {
+        "nombre": "Rana gigante del Titicaca",
+        "img_url": "https://ciorg.imgix.net/images/default-source/non-vault-images/rana-gigante-del-titicaca.jpg?sfvrsn=5dacf2ff_6&auto=compress&auto=format&w=1200&h=630",
+        "tipo": "Anfibio",
+        "region": "Lago Titicaca, Departamento de La Paz"
+      },
+      {
+        "nombre": "Puma",
+        "img_url": "https://www.lostiempos.com/sites/default/files/styles/noticia_detalle/public/media_imagen/2018/2/5/foto_2_pagina_9_nat_geo.jpg?itok=tarOU5DK",
+        "tipo": "Mamífero",
+        "region": "Yungas, Departamento de La Paz"
+      },
+      {
+        "nombre": "Ocelote",
+        "img_url": "https://www.reportechaco.com/storage/posts/December2023/B4zoAIpwSnONbCB1EYIo.webp",
+        "tipo": "Mamífero",
+        "region": "Chiquitania, Departamento de Santa Cruz"
+      },
+      {
+        "nombre": "Coatí",
+        "img_url": "https://www.barrameda.com.ar/wp-content/uploads/2019/11/coati.jpg",
+        "tipo": "Mamífero",
+        "region": "Amazonía boliviana, Departamento de Pando"
+      },
+      {
+        "nombre": "Tucán toco",
+        "img_url": "https://cdn.download.ams.birds.cornell.edu/api/v2/asset/96276931/900",
+        "tipo": "Ave",
+        "region": "Chiquitania, Departamento de Santa Cruz"
+      },
+      {
+        "nombre": "Boa constrictor",
+        "img_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/B._constrictor.jpg/640px-B._constrictor.jpg",
+        "tipo": "Reptil",
+        "region": "Amazonía boliviana, Departamento de Beni"
+      },
+      {
+        "nombre": "Mono tití",
+        "img_url": "https://media-cdn.tripadvisor.com/media/photo-s/06/38/bc/af/refugio-zoologico.jpg",
+        "tipo": "Mamífero",
+        "region": "Amazonía boliviana, Departamento de Pando"
+      },
+      {
+        "nombre": "Ciervo de los pantanos",
+        "img_url": "https://www.sanfernandonuestro.com.ar/wp/wp-content/uploads/2018/11/ciervo-e1543286866996.jpg",
+        "tipo": "Mamífero",
+        "region": "Pantanal boliviano, Departamento de Santa Cruz"
+      },
+      {
+        "nombre": "Tortuga de la Amazonía",
+        "img_url": "https://imgs.mongabay.com/wp-content/uploads/sites/25/2018/06/22170247/Andre%CC%81-Baertschi-motelo.jpg",
+        "tipo": "Reptil",
+        "region": "Amazonía boliviana, Departamento de Beni"
+      },
+      {
+        "nombre": "Guanaco",
+        "img_url": "https://www.rewildingchile.org/web/wp-content/uploads/2021/08/Guanaco_Ingo_Arndt-01-1920x1280.jpg",
+        "tipo": "Mamífero",
+        "region": "Altiplano, Departamento de Potosí"
+      },
+      {
+        "nombre": "Armadillo gigante",
+        "img_url": "https://imgs.mongabay.com/wp-content/uploads/sites/25/2021/01/28211712/armadillo-gigante.jpg",
+        "tipo": "Mamífero",
+        "region": "Chiquitania, Departamento de Santa Cruz"
+      },
+      {
+        "nombre": "Águila harpía",
+        "img_url": "https://www.anipedia.net/imagenes/habitat-del-aguila-arpia.jpg",
+        "tipo": "Ave",
+        "region": "Amazonía boliviana, Departamento de Pando"
+      },
+      {
+        "nombre": "Oso hormiguero gigante",
+        "img_url": "https://wwwlaregionbo3b375.zapwp.com/q:intelligent/r:0/wp:1/w:1/u:https://www.laregion.bo/wp-content/uploads/2023/12/Oso-hormiguero.jpg",
+        "tipo": "Mamífero",
+        "region": "Chiquitania, Departamento de Santa Cruz"
+      },
+      {
+        "nombre": "Lagarto overo",
+        "img_url": "https://imagenes.montevideo.com.uy/imgnoticias/202012/749233.jpg",
+        "tipo": "Reptil",
+        "region": "Chaco boliviano, Departamento de Tarija"
+      },
+      {
+        "nombre": "Guacamayo rojo",
+        "img_url": "https://picturebirdai.com/image-handle/image/1080/154010762703011867.jpeg?x-oss-process=image/format,webp",
+        "tipo": "Ave",
+        "region": "Amazonía boliviana, Departamento de Beni"
+      },
+      {
+        "nombre": "Perezoso de tres dedos",
+        "img_url": "https://www.shutterstock.com/image-photo/threetoed-sloth-bradypodidae-hangs-on-600nw-2505389695.jpg",
+        "tipo": "Mamífero",
+        "region": "Amazonía boliviana, Departamento de Pando"
+      },
+      {
+        "nombre": "Delfín rosado",
+        "img_url": "https://bolivialove.com/wp-content/uploads/2022/08/ruta-del-bufeo-delfin-rosa-delfin-de-agua-dulce-delfin-de-rio.jpg",
+        "tipo": "Mamífero",
+        "region": "Ríos de la Amazonía, Departamento de Beni"
+      },
+      {
+        "nombre": "Carpincho",
+        "img_url": "https://accionyreaccion.com/wp-content/themes/patterns/timthumb.php?src=https%3A%2F%2Faccionyreaccion.com%2Fwp-content%2Fuploads%2F2019%2F07%2FMadreCarpincho.jpg&q=90&w=650&zc=1",
+        "tipo": "Mamífero",
+        "region": "Pantanal boliviano, Departamento de Santa Cruz"
+      }
+    ]
+  }
+
+  export default biodiversity;
